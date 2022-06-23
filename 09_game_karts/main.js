46b744441f57fef878aee904046d0f95653431cc
Vlad Em <@haos1996haos@gmail.com>
2022-06-23 12:54:14 +0300

    size cards, start window

--- 09_game_karts/main.js
+++ 09_game_karts/main.js
@@ -32,87 +32,148 @@ document.addEventListener("DOMContentLoaded", function () {
     //добавление карточек с парными идентификаторами
     for (let amount = 0; amount < amountCard; amount++) {
       let card = document.createElement("div");
+      let span = document.createElement("span");
       card.classList.add("card");
       card.classList.add("visible_none");
       cards.append(card);
-      card.textContent = arrCards[amount];
+      card.append(span);
+      span.textContent = arrCards[amount];
     }
 
     document.body.append(cards);
     console.log(arrCards); //вывод массива карточек в начале игры
+    eventCard();
   }
 
-  createCards(16);
+  //
+  function startGame() {
+    let groupEl = document.createElement("div");
+    let input = document.createElement("input");
+    let buttonStart = document.createElement("button");
+    let label = document.createElement("label");
+    let error = document.createElement("span");
+    groupEl.classList.add("groupEl");
+    input.classList.add("input");
+    buttonStart.classList.add("button");
+    label.classList.add("label");
+    error.classList.add("error");
 
-  let card = document.querySelectorAll(".card");
-  let cardActive = false;
-  let numCard = null;
-  let contentCardActive = null;
-  card.forEach(function (cardClick) {
-    cardClick.addEventListener("click", function () {
-      if (!cardActive) {
-        console.log(`нажата первая карточка ${cardClick.textContent}`);
-        cardClick.classList.add("open_animation");
+    input.placeholder = "8";
+    input.type = "number";
+    input.max = "50";
+    input.min = "2";
+    label.textContent = "Введите кол-во пар карточек (2-50):";
+    buttonStart.textContent = "Поехали!";
+    groupEl.append(label);
+    groupEl.append(input);
+    groupEl.append(buttonStart);
+    groupEl.append(error);
+    document.body.append(groupEl);
 
-        function openOneCard() {
-          cardClick.classList.remove("visible_none");
-          numCard = cardClick.textContent;
-          cardClick.classList.add("card_active");
-          cardActive = !cardActive;
-          cardClick.classList.add("pointer");
-          contentCardActive = cardClick.textContent;
+    buttonStart.addEventListener("click", function () {
+      let amountCards = input.value;
+      let sizeCard = 600;
+      if (amountCards > 50 || amountCards < 2 || amountCards.length === 0) {
+        error.textContent = "Недопустимое значение!";
+        error.classList.remove("error_no_visible");
+        error.classList.add("error_visible");
+      } else {
+        error.classList.remove("error_visible");
+        error.classList.add("error_no_visible");
+        createCards(amountCards * 2);
+        groupEl.remove();
+
+        //рассчет размера карточки от их кол-ва
+        for (let num = 0; num < amountCards; ) {
+          if (
+            document.body.clientHeight - document.documentElement.clientHeight >
+            0
+          ) {
+            sizeCard -= 5;
+            cards.style.setProperty("--size-card", sizeCard + "px");
+          } else {
+            num += 1;
+          }
         }
-        setTimeout(() => openOneCard(), 150);
-      } else if (cardActive) {
-        console.log(`нажата вторая карточка ${cardClick.textContent}`);
-        cardClick.classList.add("open_animation");
-
-        if (cardClick.textContent === numCard) {
-          console.log("карточки совпадают");
-
-          function openTwoCardIdent() {
-            cardClick.classList.add("card_active");
-            card.forEach(function (el) {
-              if (el.textContent === contentCardActive) {
-                console.log(el.textContent);
-                el.classList.add("cards_open");
-                el.classList.add("pointer");
-                el.classList.remove("visible_none");
-              }
-            });
-          }
-          setTimeout(() => openTwoCardIdent(), 150);
-        } else {
-          console.log("Карточки не совпадают");
-
-          cardClick.classList.add("open_animation");
-
-          function openTwoCard() {
-            cardClick.classList.remove("visible_none");
-            cardClick.classList.add("card_active");
-            cards.classList.add("pointer");
-          }
-          setTimeout(() => openTwoCard(), 150);
-
-          function closeCard() {
-            cardClick.classList.add("visible_none");
-            card.forEach(function (el) {
-              if (el.textContent === contentCardActive) {
-                cardClick.classList.remove("card_active");
-                cardClick.classList.remove("open_animation");
-                el.classList.remove("card_active");
-                el.classList.remove("pointer");
-                el.classList.add("visible_none");
-                el.classList.remove("open_animation");
-              }
-            });
-            cards.classList.remove("pointer");
-          }
-
-          setTimeout(() => closeCard(), 1000);
-        }
-        cardActive = !cardActive;
       }
     });
-  });
+  }
+
+  function eventCard() {
+    let card = document.querySelectorAll(".card");
+    let cardActive = false;
+    let numCard = null;
+    let contentCardActive = null;
+    card.forEach(function (cardClick) {
+      cardClick.addEventListener("click", function () {
+        if (!cardActive) {
+          console.log(`нажата первая карточка ${cardClick.textContent}`);
+          cardClick.classList.add("open_animation");
+
+          function openOneCard() {
+            cardClick.classList.remove("visible_none");
+            numCard = cardClick.textContent;
+            cardClick.classList.add("card_active");
+            cardActive = !cardActive;
+            cardClick.classList.add("pointer");
+            contentCardActive = cardClick.textContent;
+          }
+          setTimeout(() => openOneCard(), 150);
+        } else if (cardActive) {
+          console.log(`нажата вторая карточка ${cardClick.textContent}`);
+          cardClick.classList.add("open_animation");
+
+          if (cardClick.textContent === numCard) {
+            console.log("карточки совпадают");
+
+            function openTwoCardIdent() {
+              cardClick.classList.add("card_active");
+              card.forEach(function (el) {
+                if (el.textContent === contentCardActive) {
+                  console.log(el.textContent);
+                  el.classList.add("cards_open");
+                  el.classList.add("pointer");
+                  el.classList.remove("visible_none");
+                }
+              });
+            }
+            setTimeout(() => openTwoCardIdent(), 150);
+          } else {
+            console.log("Карточки не совпадают");
+
+            cardClick.classList.add("open_animation");
+
+            function openTwoCard() {
+              cardClick.classList.remove("visible_none");
+              cardClick.classList.add("card_active");
+              cards.classList.add("pointer");
+            }
+            setTimeout(() => openTwoCard(), 150);
+
+            function closeCard() {
+              cardClick.classList.add("visible_none");
+
+              card.forEach(function (el) {
+                if (el.textContent === contentCardActive) {
+                  cardClick.classList.remove("card_active");
+                  cardClick.classList.remove("open_animation");
+                  el.classList.remove("card_active");
+                  el.classList.remove("pointer");
+                  el.classList.add("visible_none");
+                  el.classList.remove("open_animation");
+                }
+              });
+              cards.classList.remove("pointer");
+            }
+
+            setTimeout(() => closeCard(), 700);
+          }
+          cardActive = !cardActive;
+        }
+      });
+    });
+  }
+
+  startGame();
+  // createCards(16);
 });
